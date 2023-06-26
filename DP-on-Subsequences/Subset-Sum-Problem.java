176fbda91eaed87c9799407ebe1e89fe13ceb824
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-06-26 16:37:16 -0400

    Update Subset-Sum-Problem.java

--- DP-on-Subsequences/Subset-Sum-Problem.java
+++ DP-on-Subsequences/Subset-Sum-Problem.java
@@ -32,3 +32,99 @@ Constraints:
 1<= arr[i] <= 100
 1<= sum <= 105
 */
+
+class Solution{
+
+    static boolean isSubsetSum(int N, int arr[], int sum){
+        // code here
+        //return isSubsetSumByRecursion(N - 1, arr, sum);
+        /*
+        int[][] dp = new int[N][sum + 1];
+        for(int i = 0; i < N; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return isSubsetSumByMemoization(N - 1, arr, sum, dp);
+        */
+        boolean[][] dp = new boolean[N][sum + 1];
+        return isSubsetSumByTabulation(N, arr, sum, dp);
+    }
+    
+    static boolean isSubsetSumByRecursion(int i, int arr[], int sum){
+        // code here
+        if(sum == 0){
+            return true;
+        }
+        if(i == 0){
+            if(sum == arr[i]){
+                return true;
+            }
+            return false;
+            
+        }
+        boolean pick = isSubsetSumByRecursion(i - 1, arr, sum - arr[i]);
+        boolean notPick = isSubsetSumByRecursion(i - 1, arr, sum);
+        return pick || notPick;
+    }
+    
+    static boolean isSubsetSumByMemoization(int i, int arr[], int sum, int[][] dp){
+        // code here
+        if(sum == 0){
+            return true;
+        }
+        if(i == 0){
+            if(sum == arr[i]){
+                return true;
+            }
+            return false;
+            
+        }
+        if(dp[i][sum] != -1){
+            if(dp[i][sum] == 1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        boolean pick = false;
+        if(sum >= arr[i]){
+            pick = isSubsetSumByMemoization(i - 1, arr, sum - arr[i], dp);
+        }
+        boolean notPick = isSubsetSumByMemoization(i - 1, arr, sum, dp);
+        if(pick || notPick){
+            dp[i][sum] = 1;
+            return true;
+        }
+        else{
+            dp[i][sum] = 0;
+            return false;
+        }
+    }
+    
+    static boolean isSubsetSumByTabulation(int n, int arr[], int sum, boolean[][] dp){
+        // code here
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+        if(sum >= arr[0]){
+            dp[0][arr[0]] = true;
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= sum; j++){
+                boolean pick = false;
+                if(j >= arr[i]){
+                    pick = dp[i - 1][j - arr[i]];
+                }
+                boolean notPick = dp[i - 1][j];
+                if(pick || notPick){
+                    dp[i][j] = true;
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n - 1][sum];
+    }
+}
